7099c983eb7d303c619215455692a9fcad3dbdc8
Himanshu Sekhar Das <61196311+dasHimanshuSekhar@users.noreply.github.com>
2026-04-16 11:29:17 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        Map<Integer, Integer> mem = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i ++){
+            mem.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i ++){
+            int complementSum;
+            if(target > 0 || nums[i] > 0){
+                complementSum = Math.abs(target - nums[i]);
+            }
+            complementSum = target - nums[i];
+            // System.out.println("complementSum: " + complementSum);
+            if(mem.containsKey(complementSum) && mem.get(complementSum) != i) {
+                ans[0] = i;
+                ans[1] = mem.get(complementSum);
+                break;
+            }
+        }
+        return ans;
+    }
+}
